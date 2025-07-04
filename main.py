8af26f996665e736092b714de5fc53c4c0358389
tongcai deng <dengtongcai@gmail.com>
2025-07-04 22:21:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2442,17 +2442,11 @@ class NewsAnalyzer:
         print(f"报告类型: {self.report_type}")
 
         ids = [
-            ("toutiao", "今日头条"),
-            ("baidu", "百度热搜"),
             ("wallstreetcn-hot", "华尔街见闻"),
             ("thepaper", "澎湃新闻"),
-            ("bilibili-hot-search", "bilibili 热搜"),
             ("cls-hot", "财联社热门"),
             ("ifeng", "凤凰网"),
-            "tieba",
-            "weibo",
             "douyin",
-            "zhihu",
         ]
 
         print(f"开始爬取数据，请求间隔 {self.request_interval} 毫秒")
